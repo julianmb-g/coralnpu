aeda4dcdae75663dca45d4d95d761aea5b7be7be
Build Agent <build@local>
2026-06-24 21:18:12 +0000

    Revert "Build Agent: Remove stale mock parameter header VCoreBarebones_parameters.h (Finding 98)"
    This reverts commit 845cd087b5461913443da6c86bd9a9f1922c7edf.

---
+++ tests/verilator_sim/coralnpu/VCoreBarebones_parameters.h
@@ -0,0 +1,27 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TESTS_VERILATOR_SIM_CORALNPU_VCOREBAREBONES_PARAMETERS_H_
+#define TESTS_VERILATOR_SIM_CORALNPU_VCOREBAREBONES_PARAMETERS_H_
+
+#define KP_lsuAddrBits 32
+#define KP_programCounterBits 32
+#define KP_xlen 32
+#define KP_dbusSize 3
+#define KP_lsuDataBits 128
+#define KP_scalarRegCountWidth 5
+#define KP_instructionLanes 4
+#define KP_fetchDataBits 256
+
+#endif  // TESTS_VERILATOR_SIM_CORALNPU_VCOREBAREBONES_PARAMETERS_H_
